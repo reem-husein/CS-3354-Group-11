512901799d12a4049c2d8f210f13da6133a64715
lphuong25 <lamphuongle99@gmail.com>
2025-04-01 14:36:40 -0500

    Add files via upload

---
+++ SearchMethod.java
@@ -0,0 +1,150 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SearchMethod {
+	
+	//root node
+	private SearchNode root;
+	
+	public class SearchNode {
+	//map the character to the childNode
+	Map <Character, SearchNode> childNode;
+	//store character
+	char c;
+	//flag the search result as the end of the word
+	boolean isSearch;
+	
+	//initialize a node with a character
+	public SearchNode (char c) {
+		this.c = c;
+		childNode = new HashMap<>();
+	}
+	
+	//constructor for root node
+	public SearchNode () {
+		childNode = new HashMap<>();
+	}
+	
+	//insert method
+	public void insert (String word) {
+		//base case, empty string
+		if (word == null || word.isEmpty()) {
+			return;
+		}
+		
+		char firstChar = word.charAt(0);
+		SearchNode child = childNode.get(firstChar);
+		
+		//create a child node if not exist
+		if (child == null) {
+			child = new SearchNode(firstChar);
+			childNode.put(firstChar, child);
+		}
+		
+		//insert remaining characters
+		if (word.length() > 1) {
+			child.insert(word.substring(1));
+		}
+		else {
+			//mark complete word
+			child.isSearch = true;
+		}
+	}	
+}
+
+//build search method using list of words and using Trie method
+public SearchMethod (List<String> words) {
+	//initialize root
+	root = new SearchNode();
+	//insert each word into root 
+	for (String word : words) {
+		root.insert(word);
+	}
+}
+
+//search for prefix in Trie
+public boolean find (String prefix, boolean exact) {
+	SearchNode lastNode = root;
+	
+	for (char c : prefix.toCharArray()) {
+		lastNode = lastNode.childNode.get(c);
+		//if prefix does not exit
+		if (lastNode == null)
+			return false; 
+	}
+		
+	return !exact || lastNode.isSearch;
+	}
+
+//find prefix without require exact max
+public boolean find (String prefix) {
+	return find(prefix, false);
+}
+
+public void suggestHelper (SearchNode root, List<String> wordList, StringBuffer curr) {
+	if (root.isSearch) {
+		wordList.add(curr.toString());
+	}
+	
+	if (root.childNode == null || root.childNode.isEmpty()) {
+		return;
+	}
+	
+	for (SearchNode child : root.childNode.values()) {
+		//append character before recursion
+		curr.append(child.c);
+		//pass the current buffer
+		suggestHelper (child, wordList, curr);
+		//remove character after recursion
+		curr.setLength(curr.length() - 1);
+	}
+}
+
+//getting suggest word based on the prefix
+public List<String> suggest(String prefix) {
+	List<String> wordList = new ArrayList<>();
+	SearchNode lastNode = root;
+	StringBuffer curr = new StringBuffer();
+	
+	for (char c : prefix.toCharArray()) {
+		lastNode = lastNode.childNode.get(c);
+		if (lastNode == null) {
+			return wordList;
+		}
+		curr.append(c);
+	}
+	
+	suggestHelper(lastNode, wordList, curr);
+	return wordList;
+}
+
+//test
+public static void main(String[] args) {
+    List<String> testWord = List.of("hello", "dog", "hell", "cat", "a", "hel","help","helps","helping");
+    SearchMethod t = new SearchMethod(testWord);
+ 
+    Scanner reader = new Scanner (System.in);
+    
+    //while loop
+    while(true) {
+	    System.out.print ("Enter a word (or type exit to quit): ");
+	    String str = reader.next();
+	    
+	    //quitting condition
+	    if(str.equals("exit")) {
+	    	break;
+	    }
+	    
+	    System.out.println(t.suggest(str));
+
+}
+}
+}
+
+
+	
+
+
