549b461a4593d2052647a24e36878fb9705fe015
sir-axel <niy.axel@gmail.com>
2025-04-17 13:33:45 -0500

    name changes and cleaned directories

--- project-root/server/src/main/java/com/chauffeurcheck/backend/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.chauffeurcheck.backend.repository;
-
-import com.chauffeurcheck.backend.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
